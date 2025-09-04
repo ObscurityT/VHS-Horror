8f90545d0743337f4649e11dd4c520dc807dd75e
Nathy-Brito <nathbrito19@gmail.com>
2025-09-04 12:07:11 -0300

    add: window rule adjustments

--- Assets/Scripts/Audio/AudioManager.cs
+++ Assets/Scripts/Audio/AudioManager.cs
@@ -43,9 +43,14 @@ namespace AudioSystem
         public void PlaySFX(string name)
         {
             if (sfxDict.ContainsKey(name))
+            {
+                Debug.Log("[AudioManager] Tocando SFX: " + name);
+                // sfxSource.spatialBlend = 0f; // forÃ§a som 2D
+                // sfxSource.volume = 1f;       // forÃ§a volume mÃ¡ximo
                 sfxSource.PlayOneShot(sfxDict[name]);
+            }
             else
-                Debug.LogWarning("SFX não encontrado: " + name);
+                Debug.LogWarning("SFX nï¿½o encontrado: " + name);
         }
 
         public void PlayMusic(string name, bool loop = true)
@@ -58,7 +63,7 @@ namespace AudioSystem
             }
             else
             {
-                Debug.LogWarning("Música não encontrada: " + name);
+                Debug.LogWarning("Mï¿½sica nï¿½o encontrada: " + name);
             }
         }
 
--- Assets/Scripts/Rules/WindowRule/WolfTrigger.cs
+++ Assets/Scripts/Rules/WindowRule/WolfTrigger.cs
@@ -6,25 +6,16 @@ using UnityEngine;
 public class WolfTrigger : MonoBehaviour
 {
     [Header("Áudio")]
-    public AudioClip wolfClip;
+    public string wolfSFXName = "LoboUivo";
     [Range(0f, 1f)] public float volume = 1f;
- 
+
     [Header("Parâmetros")]
     public string playerTag = "Player";
     public string windowTag = "Window";
     public string closeParameter = "Close"; //aqui vem o nome do parametro que tem no animator do unity na animação de fechar a janela
     public bool useTriggerParameter = true;
 
-    AudioSource audioSource;
     bool triggered = false;
-
-    void Awake()
-    {
-        audioSource = GetComponent<AudioSource>();
-        audioSource = gameObject.AddComponent<AudioSource>();
-        audioSource.playOnAwake = false;
-        audioSource.spatialBlend = 1f;
-    }
 
     void OnTriggerEnter(Collider other)
     {
@@ -33,29 +24,31 @@ public class WolfTrigger : MonoBehaviour
 
         triggered = true;
         PlayWolf();
-        CloseAllWindows();
-        
+        // CloseAllWindows(); reativar depois, quando tiver a animação anexada
+        Debug.Log("[WolfTrigger] Player entrou na área do lobo. Toca o som e fecha as janelas.");
+
     }
 
     void PlayWolf()
     {
-        audioSource.PlayOneShot(wolfClip, volume);
+        AudioManager.Instance.PlaySFX("LoboUivo");
     }
 
-    void CloseAllWindows()
-    {
-        var windows = GameObject.FindGameObjectsWithTag(windowTag);
-        foreach (var w in windows)
-        {
-            var animator = w.GetComponent<Animator>();
-            if (useTriggerParameter)
-            {
-                animator.SetTrigger(closeParameter);
-            }
-            else
-            {
-                animator.SetBool(closeParameter, true);
-            }
-        }
-    }
+// reativar essa método depois, quando tiver a animação anexada
+    // void CloseAllWindows()
+    // {
+    //     var windows = GameObject.FindGameObjectsWithTag(windowTag);
+    //     foreach (var w in windows)
+    //     {
+    //         var animator = w.GetComponent<Animator>();
+    //         if (useTriggerParameter)
+    //         {
+    //             animator.SetTrigger(closeParameter);
+    //         }
+    //         else
+    //         {
+    //             animator.SetBool(closeParameter, true);
+    //         }
+    //     }
+    // }
 }
